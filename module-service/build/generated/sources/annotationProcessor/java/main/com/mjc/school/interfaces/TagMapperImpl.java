eac53c46346bea4991775212824831806b635e87
= <=>
2023-06-14 15:22:39 +0500

    Spring with Rest final.

--- module-service/build/generated/sources/annotationProcessor/java/main/com/mjc/school/interfaces/TagMapperImpl.java
+++ module-service/build/generated/sources/annotationProcessor/java/main/com/mjc/school/interfaces/TagMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-11T19:45:20+0400",
+    date = "2023-06-14T14:20:41+0400",
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.jar, environment: Java 17.0.6 (JetBrains s.r.o.)"
 )
 @Component
