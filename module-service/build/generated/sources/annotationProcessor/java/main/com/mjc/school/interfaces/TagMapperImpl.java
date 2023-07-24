7cfa4defc712eaed74a0873eedb650a57444efff
= <=>
2023-07-24 18:17:07 +0500

    SonarQube

--- module-service/build/generated/sources/annotationProcessor/java/main/com/mjc/school/interfaces/TagMapperImpl.java
+++ module-service/build/generated/sources/annotationProcessor/java/main/com/mjc/school/interfaces/TagMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-07-24T17:00:00+0400",
-    comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.jar, environment: Java 17.0.6 (JetBrains s.r.o.)"
+    date = "2023-07-24T17:04:12+0400",
+    comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.jar, environment: Java 20 (Oracle Corporation)"
 )
 @Component
 public class TagMapperImpl implements TagMapper {
